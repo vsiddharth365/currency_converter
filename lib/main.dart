45ff982dbebd4a69eb82e0a1f295381874d2d6ad
vsiddharth <vsiddharth365@gmail.com>
2023-11-28 12:37:38 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:currency_converter/pages/currency_converter_material_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CurrencyConverterMaterialPage(),
+    );
+  }
+}
